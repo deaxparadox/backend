34cfebd035f7408b045488d44f0d71fa9fffaee4
deaxparadox <nitish136900kushwaha@gmail.com>
2023-06-11 05:46:37 +0530

    youtube: blog: models and admin: views, urls and templates

--- Blog/backend/blog/admin.py
+++ Blog/backend/blog/admin.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
-import typing 
 
-from .models import Post, Comment
+from .models import Post
 
-@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = [
-        "title", "slug", "author", "publish", "status"
-    ]
-    list_filter = ['status', 'created', 'publish', 'author']
-    search_fields = ['title', 'body']
-    prepopulated_fields = {"slug": ("title", )}
-    raw_id_fields = ["author"]
-    date_hierarchy = "publish"
-    ordering = ['status', 'publish']
+    list_display = ("title", "created", "published", "status",)
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
 
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'post', 'created', 'active']
-    list_fitler = ['active', 'created', 'updated']
-    search_fields = ['name', 'email', 'body']
+admin.site.register(Post, PostAdmin)
--- Blog/backend/blog/urls.py
+++ Blog/backend/blog/urls.py
@@ -1,26 +1,10 @@
-from django.urls import path 
+from django.urls import path
 
-from . import views 
-from api import apiview
-from .feeds import LatestPostsFeed
-
+from . import views
 
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    path(
-        "<int:year>/<int:month>/<int:day>/<slug:post>/", 
-        views.post_detail, 
-        name="post_detail"
-    ),
-    path("<int:post_id>/share/", views.post_share, name="post_share"),
-    path("<int:post_id>/comment/", views.post_comment, name="post_comment"),
-    path("tag/<slug:tag_slug>/", views.post_list, name="post_list_by_tag"),
-    path("feed/", LatestPostsFeed(), name="post_feed"),
-    path("search/", views.post_search, name="post_search"),
-
-    # API
-    path("api/", apiview.BlogListView.as_view(), name="api_post_list"),
-    path("api/create/", apiview.BlogCreateView.as_view(), name="api_post_create")
+    path("", views.index, name="index"),
+    path("<int:id>/<slug:slug>/", views.detail, name="detail")
 ]
