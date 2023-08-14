b4e9934573e83533de9e133fa3d85878bd369440
deaxparadox <nitish136900kushwaha@gmail.com>
2023-08-15 03:47:38 +0530

    django: only

---
+++ mpa/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+console.log(__dirname)
+
+module.exports = {
+  entry: {
+    "base": __dirname + "/static/ts/base.ts",
+    "linux": __dirname + "/static/ts/linux.ts",
+    "python": __dirname + "/static/ts/python.ts",    
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'static/js/'),
+  },
+};
