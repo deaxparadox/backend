b21a98d058264cf702cd641db0048491c4b4ebc1
deaxparadox <nitish136900kushwaha@gmail.com>
2023-08-18 15:31:18 +0530

    trying to remove migrations files

---
+++ django/wagtail_cms/wagtail_cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-l=6sjf1l+bw3ynbxwaq$+&-ae-a=+w1$+dvga(t!maj6y6qihv"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
