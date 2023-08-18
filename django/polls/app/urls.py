6cd101611ad431b98693c3e6393f24dd2c01262d
deaxparadox <nitish136900kushwaha@gmail.com>
2023-08-18 21:28:48 +0530

    django: polls

--- django/polls/app/urls.py
+++ django/polls/app/urls.py
@@ -1,17 +1,38 @@
 from django.urls import path
 
 from . import views
+from . import generic_views
 
-urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
- 
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
- 
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
- 
-    # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+app_name = "app"
+
+try:
+    urlpatterns = [
+        # ex: /polls/
+        path("", generic_views.IndexView.as_view(), name="index"),
+    
+        # ex: /polls/5/
+        path("<int:pk>/", generic_views.DetailView.as_view(), name="detail"),
+    
+        # ex: /polls/5/results/
+        path("<int:pk>/results/", generic_views.ResultsView.as_view(), name="results"),
+
+    ]
+except:
+
+    urlpatterns = [
+        # ex: /polls/
+        path("", views.index, name="index"),
+    
+        # ex: /polls/5/
+        path("<int:pk>/", views.detail, name="detail"),
+    
+        # ex: /polls/5/results/
+        path("<int:pk>/results/", views.results, name="results"),
+    
+       
+    ]
+
+urlpatterns += [
+     # ex: /polls/5/vote/
+    path("<int:pk>/vote/", views.vote, name="vote"),
 ]
--- django/polls/app/views.py
+++ django/polls/app/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -10,14 +11,36 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "app/index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+def detail(request, pk):
+    question = get_object_or_404(Question, pk=pk)
     return render(request, "app/detail.html", {"question": question})
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    return render(
+        request,
+        "app/results.html",
+        {
+            "question": question
+        }
+    )
 
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def vote(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "app/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice."
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("app:results", args=(question.id,)))
