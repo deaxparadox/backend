1a78db08d2ebc9443ba9577d89adddfc6cdde997
deaxparadox <nitish136900kushwaha@gmail.com>
2023-09-22 04:51:11 +0530

    setenv script : not working

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+# creating environ
+# activating environment
+# updating pip 
+# install packages
+
+REQUIREMENTS_DJANGO: str = "assets/requirements_django.txt"
+REQUIREMENTS_FASTAPI: str = "assets/requirements_fastapi.txt"
+
+
