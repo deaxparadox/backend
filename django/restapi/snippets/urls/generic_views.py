bcde75a960432ab6ded0e981380d2f9c1edd8498
deaxparadox <nitish136900kushwaha@gmail.com>
2023-10-09 21:15:02 +0530

    removed everything

--- django/restapi/snippets/urls/generic_views.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from snippets.views import generic_views
-
-app_name = "snippet_generic_views"
-
-urlpatterns = [
-    path("snippets/", generic_views.SnippetList.as_view()),
-    path("snippets/<int:pk>/", generic_views.SnippetDetail.as_view()),
-]
--- tools/celery/104-application/base.py
+++
@@ -1,6 +0,0 @@
-def fibonacci(num: int | float) -> int:
-    if num == 1:
-        return 0
-    elif num == 2:
-        return 1
-    return fibonacci(num-1) + fibonacci(num-2)
