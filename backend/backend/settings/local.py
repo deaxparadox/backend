813934b260fff7fb99b2f9794877d4bab917cd11
deaxparadox <nitish136900kushwaha@gmail.com>
2024-05-14 13:48:57 +0530

    working on todo application

---
+++ backend/backend/settings/local.py
@@ -0,0 +1,10 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-_whye!!lzfzt2^#)ndydi12%#na3vmdtz&ne_%@qe0o)(ld65p'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
